f949f4e0a28486a19785dbbfc05f7bbf876b9e97
Marijn Heuts <m.heuts@live.nl>
2019-04-07 23:21:02 +0200

    Game Over state

--- Machiavelli-VS/GameOverState.h
+++ Machiavelli-VS/GameOverState.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "State.h"
+
+class GameOverState: public State
+{
+public:
+	void enter_state() override;
+private:
+	void showScoreScreen(const std::shared_ptr<ClientInfo> client);
+	int calculateBuildingScore(const std::shared_ptr<ClientInfo> client);
+	int calculateCityScore(const std::shared_ptr<ClientInfo> client);
+	int calculateColorScore(const std::shared_ptr<ClientInfo> client);
+};
